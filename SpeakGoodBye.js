98283b5095dd1825931635dfed80fec561cea440
yaswanth9399 <83900007+yaswanth9399@users.noreply.github.com>
2021-07-25 16:37:17 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,4 +1,11 @@
-var speakWord = "Good Bye";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+
+
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
